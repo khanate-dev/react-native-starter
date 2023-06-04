034e5888b572cd4b7b81caedbd2e3cb248038012
SuperKXT <superkxt@outlook.com>
2023-06-04 19:57:42 +0500

    Update `babel.config.js`

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,34 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+/** @type {(import('@babel/core').ConfigFunction)} */
+module.exports = function (api) {
+	api.cache.using(() => process.env.ENV);
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			'react-native-reanimated/plugin',
+			[
+				'module-resolver',
+				{
+					alias: {
+						assets: './assets',
+						components: './src/components',
+						config: './src/config',
+						contexts: './src/contexts',
+						errors: './src/errors',
+						helpers: './src/helpers',
+						hooks: './src/hooks',
+						media: './src/media',
+						navigation: './src/navigation',
+						screens: './src/screens',
+						styles: './src/styles',
+						types: './src/types',
+						mocks: './src/mocks',
+						schemas: './src/schemas',
+						endpoints: './src/endpoints',
+						env: './src/env',
+					},
+					extensions: ['.js', '.jsx', '.ts', '.tsx', 'json'],
+				},
+			],
+		],
+	};
 };
