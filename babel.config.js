3f464f34c281e830cb44ecc508e0576e98097ac3
SuperKXT <superkxt@outlook.com>
2023-06-04 22:09:45 +0500

    Automate path alias mapping in `babel.config.js`

--- babel.config.js
+++ babel.config.js
@@ -5,35 +5,7 @@ module.exports = function (api) {
 		presets: ['babel-preset-expo'],
 		plugins: [
 			'react-native-reanimated/plugin',
-			[
-				'module-resolver',
-				{
-					alias: {
-						assets: './assets',
-						components: './src/components',
-						config: './src/config',
-						contexts: './src/contexts',
-						errors: './src/errors',
-						helpers: './src/helpers',
-						hooks: './src/hooks',
-						media: './src/media',
-						navigation: './src/navigation',
-						screens: './src/screens',
-						styles: './src/styles',
-						types: './src/types',
-						mocks: './src/mocks',
-						schemas: './src/schemas',
-						endpoints: './src/endpoints',
-						env: './src/env',
-					},
-					extensions: ['.js', '.jsx', '.ts', '.tsx', 'json'],
-				},
-			],
+			'tsconfig-paths-module-resolver',
 		],
-		env: {
-			production: {
-				plugins: ['react-native-paper/babel'],
-			},
-		},
 	};
 };
