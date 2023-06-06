b00f15a7f5423c21b7ee379ea2a389540cd64fa3
SuperKXT <superkxt@outlook.com>
2023-06-06 20:13:06 +0500

    Update typescript definition file

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,4 +1,30 @@
+/* eslint-disable import/no-default-export */
 export declare global {
 	/** global type alias for a generic object type */
 	type Obj = Record<string, unknown>;
+}
+
+declare module '*.mp3' {
+	const value: string;
+	export default value;
+}
+
+declare module '*.wav' {
+	const value: string;
+	export default value;
+}
+
+declare module '*.jpg' {
+	const value: string;
+	export default value;
+}
+
+declare module '*.png' {
+	const value: string;
+	export default value;
+}
+
+declare module '*.svg' {
+	const value: string;
+	export default value;
 }
