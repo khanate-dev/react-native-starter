9b869c7f885dfeec3efa8c80a7238503ba2233dd
SuperKXT <superkxt@outlook.com>
2023-06-07 15:38:43 +0500

    Delete app svg icons

--- src/components/media/icons/index.tsx
+++
@@ -1,6 +0,0 @@
-export * from './age.icon';
-export * from './bird.icon';
-export * from './fcr.icon';
-export * from './mortality.icon';
-export * from './shed.icon';
-export * from './weight.icon';
