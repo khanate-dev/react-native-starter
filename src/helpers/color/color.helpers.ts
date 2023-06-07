7d2e5a78236f7faf58981e38c3bd6ffe18302860
SuperKXT <superkxt@outlook.com>
2023-06-07 18:44:22 +0500

    Remove `getHeadingGradientProps` helper

--- src/helpers/color/color.helpers.ts
+++ src/helpers/color/color.helpers.ts
@@ -1,5 +1,3 @@
-import type { LinearGradientProps } from 'expo-linear-gradient';
-
 export const gradients = [
 	['#e66', '#eaa'],
 	['#9d9', '#2dd'],
@@ -13,16 +11,3 @@ export const getGradientByIndex = (index: number) =>
 	gradients[
 		(index % gradients.length) + Math.floor(index / gradients.length)
 	] ?? gradients[0];
-
-export const getHeadingGradientProps = (
-	theme: Record<string, string>
-): LinearGradientProps => {
-	const start = theme['color-primary-400'] ?? '#662B8C';
-	const end = theme['color-primary-200'] ?? '#ED00B9';
-
-	return {
-		colors: [start, end],
-		start: { x: 0, y: 0 },
-		end: { x: 0.1, y: 1 },
-	};
-};
