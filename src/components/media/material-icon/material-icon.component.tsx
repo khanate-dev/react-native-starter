7376a7013551dbd38ac254ba2459670a398de178
SuperKXT <superkxt@outlook.com>
2023-06-07 16:46:24 +0500

    Make `MaterialIcon` typesafe

--- src/components/media/material-icon/material-icon.component.tsx
+++ src/components/media/material-icon/material-icon.component.tsx
@@ -1,3 +1,8 @@
+/* eslint-disable no-restricted-imports */
 import Icon from '@expo/vector-icons/MaterialIcons';
+
+import type map from '@expo/vector-icons/build/vendor/react-native-vector-icons/glyphmaps/MaterialIcons.json';
+
+export type MaterialIconName = keyof typeof map;
 
 export const MaterialIcon = Icon;
