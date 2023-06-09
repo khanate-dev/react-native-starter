b5f8ce6d49442bfe0fbb95e6d32c15d40ddbefd4
SuperKXT <superkxt@outlook.com>
2023-06-09 13:08:38 +0500

    Update `user.schemas` to use `FormSchema`

--- src/schemas/user/user.schemas.ts
+++ src/schemas/user/user.schemas.ts
@@ -1,13 +1,15 @@
 import { z } from 'zod';
 
-import { createSchema, createSchemaFields, jwtSchema } from 'helpers/schema';
+import { createSchema, emailSchema, jwtSchema } from 'helpers/schema';
+
+import { FormSchema } from '..';
 
 const passwordSchema = z
 	.string()
 	.min(4, 'Password must be at least 8 characters long');
 
 export const [userSansMetaSchema, userSchema] = createSchema({
-	email: z.string().email(),
+	email: emailSchema,
 	name: z.string(),
 	password: passwordSchema,
 });
@@ -16,63 +18,53 @@ export type UserSansMeta = z.infer<typeof userSansMetaSchema>;
 
 export type User = z.infer<typeof userSchema>;
 
-export const loginSchema = userSchema.pick({
-	email: true,
-	password: true,
-});
-
 export const loggedInUserSchema = userSchema.extend({
 	token: jwtSchema,
 });
 
 export type LoggedInUser = z.infer<typeof loggedInUserSchema>;
 
+export const loginSchema = userSchema.pick({ email: true, password: true });
+
 export type Login = z.infer<typeof loginSchema>;
 
-export const [loginFields, loginFormSchema] = createSchemaFields(loginSchema, {
-	email: { type: 'email' },
-	password: { type: 'password' },
-});
-export type LoginField = keyof typeof loginFields;
-
-const [baseForgotPasswordSchema] = createSchema({
-	email: z.string().email(),
-	code: z.preprocess(
-		(value) =>
-			Number(
-				z
-					.string()
-					.regex(/[0-9]{6}/u, 'Invalid Code')
-					.parse(String(value))
-			),
-		z.number()
-	),
-	password: passwordSchema,
-	confirmPassword: passwordSchema,
+export const loginFormSchema = new FormSchema({
+	email: {
+		zod: loginSchema.shape.email,
+		type: 'email',
+	},
+	password: {
+		zod: loginSchema.shape.password,
+		type: 'password',
+	},
 });
 
-export const forgotPasswordSchema = baseForgotPasswordSchema.refine(
-	(state) => state.password === state.confirmPassword,
-	'The two passwords must match'
-);
-
-export const [forgotPasswordFields] = createSchemaFields(
-	baseForgotPasswordSchema,
-	{
-		email: {
-			type: 'email',
-		},
-		code: {
-			type: 'int',
-			label: 'Reset Code',
-		},
-		password: {
-			type: 'password',
-		},
-		confirmPassword: {
-			type: 'password',
-		},
-	}
-);
+const forgotPasswordSchema = userSchema
+	.pick({ email: true, password: true })
+	.extend({
+		code: z.number().int().min(0).max(999999),
+		confirmPassword: userSchema.shape.password,
+	});
 
 export type ForgotPassword = z.infer<typeof forgotPasswordSchema>;
+
+export const forgotPasswordFormSchema = new FormSchema({
+	email: {
+		zod: forgotPasswordSchema.shape.email,
+		type: 'email',
+	},
+	code: {
+		zod: forgotPasswordSchema.shape.code,
+		type: 'int',
+		label: 'Reset Code',
+	},
+	password: {
+		zod: forgotPasswordSchema.shape.password,
+		type: 'password',
+	},
+	confirmPassword: {
+		zod: forgotPasswordSchema.shape.confirmPassword,
+		type: 'password',
+		label: 'Confirm Password',
+	},
+});
