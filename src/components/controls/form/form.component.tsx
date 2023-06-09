0bbcf76e7bb4e6229319e46727457a3980687049
SuperKXT <superkxt@outlook.com>
2023-06-09 12:47:55 +0500

    Overhaul `Form` component to work with `FormClass`

--- src/components/controls/form/form.component.tsx
+++ src/components/controls/form/form.component.tsx
@@ -1,66 +1,50 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { Keyboard, ScrollView, View } from 'react-native';
 import { ZodError } from 'zod';
 
-import { humanizeToken } from 'helpers/string';
-import { objectEntries } from 'helpers/object';
 import { FormInput } from 'components/controls/form-input';
 import { FormButton } from 'components/controls/form-button';
 import { Alert } from 'components/feedback/alert';
 import { getCatchMessage } from 'errors/errors';
-import { shouldAutoFill } from 'src/config';
 
-import type { TextInput } from 'react-native';
-import type {
-	FormErrors,
-	FormState,
-	SchemaFields,
-	SchemaField,
-} from 'types/form';
+import type { z } from 'zod';
+import type { FormErrors } from 'types/form';
 import type { FormButtonProps } from 'components/controls/form-button';
 import type { App } from 'types/app';
 import type { AppIconName } from 'components/media/app-icon';
 import type { ThemeColor } from 'styles/theme';
-
-export const formTypeDefaults: Record<SchemaField<string>['type'], string> = {
-	string: 'test',
-	int: '25',
-	float: '25.255',
-	date: '2022-10-20T10:20:00.000Z',
-	time: '10:20',
-	email: 'testing@test.com',
-	password: '12345',
-	phone: '090078601',
-	search: '',
-};
-
-export const formDefaults: Record<string, string> = {
-	Cnic: '12345-1234567-1',
-};
+import type {
+	FormSchema,
+	FormSchemaField,
+	FormSchemaMap,
+	FormSchemaWorkingObj,
+	FormSchemaWorkingType,
+	FormSchemaFieldType,
+} from 'schemas';
 
 export type FormProps<
-	Keys extends string,
-	Fields extends SchemaFields<Keys>
+	T extends Record<string, FormSchemaMap[FormSchemaFieldType]>
 > = App.PropsWithStyle<{
 	/** the input fields object to use for the form */
-	fields: Fields;
+	schema: FormSchema<T>;
 
 	/** the default values for form inputs */
-	defaultValues?: null | Partial<FormState<keyof Fields>>;
+	defaultValues?: null | Partial<{ [k in keyof T]: z.infer<T[k]> }>;
 
 	/**
 	 * the function to call when the submit button is pressed.
 	 *
 	 * return a string from the promise to show as a success message
 	 */
-	onSubmit: (state: FormState<keyof Fields>) => Promise<void | string>;
+	onSubmit: (state: { [k in keyof T]: z.infer<T[k]> }) => Promise<
+		void | string
+	>;
 
 	/** the function to call when an input's value changes */
-	onInputChange?: (
-		name: Keys,
-		field: SchemaField<Keys>,
-		newValue: string,
-		state: FormState<keyof Fields>
+	onInputChange?: <Key extends keyof T>(
+		field: FormSchemaField<T[Key]>,
+		newValue: FormSchemaWorkingType<T[Key]>,
+		state: FormSchemaWorkingObj<T>
 	) => void;
 
 	/** the label of the submit button */
@@ -76,17 +60,19 @@ export type FormProps<
 	hasIcons?: boolean;
 
 	/** is the form page busy? */
-	isBusy?: boolean | ((state: FormState<keyof Fields>) => boolean);
+	isBusy?: boolean | ((state: FormSchemaWorkingObj<T>) => boolean);
 
 	/** should the form submission be disabled? */
-	disabled?: boolean | ((state: FormState<keyof Fields>) => boolean);
+	disabled?: boolean | ((state: FormSchemaWorkingObj<T>) => boolean);
 }>;
 
 export type Status = null | string | { type: ThemeColor; text: string };
 
-export const Form = <Keys extends string, Fields extends SchemaFields<Keys>>({
+export const Form = <
+	T extends Record<string, FormSchemaMap[FormSchemaFieldType]>
+>({
 	style,
-	fields,
+	schema,
 	defaultValues,
 	submitLabel,
 	submitIcon,
@@ -96,25 +82,12 @@ export const Form = <Keys extends string, Fields extends SchemaFields<Keys>>({
 	hasIcons,
 	isBusy,
 	disabled,
-}: FormProps<Keys, Fields>) => {
-	const formRefs = useRef<(null | TextInput)[]>([]);
-
-	const entries = objectEntries(fields);
-
-	const [form, setForm] = useState(
-		entries.reduce(
-			(object, [key, field]) => ({
-				...object,
-				[key]:
-					defaultValues?.[key] ??
-					(shouldAutoFill
-						? formDefaults[key] ?? formTypeDefaults[field.type]
-						: ''),
-			}),
-			{} as FormState<keyof Fields>
-		)
-	);
-	const [errors, setErrors] = useState<FormErrors<keyof Fields>>({});
+}: FormProps<T>) => {
+	const [form, setForm] = useState({
+		...schema.defaultValues,
+		...defaultValues,
+	});
+	const [errors, setErrors] = useState<FormErrors<keyof T>>({});
 	const [status, setStatus] = useState<Status>(null);
 	const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
 
@@ -146,7 +119,8 @@ export const Form = <Keys extends string, Fields extends SchemaFields<Keys>>({
 					const name = path[0];
 					return (
 						code === 'custom' ||
-						(typeof name === 'string' && !Object.keys(fields).includes(name))
+						(typeof name === 'string' &&
+							!Object.keys(schema.fields).includes(name))
 					);
 				});
 				const newStatus = otherIssue
@@ -164,42 +138,25 @@ export const Form = <Keys extends string, Fields extends SchemaFields<Keys>>({
 	return (
 		<View style={[{ flexGrow: 1, flexShrink: 0 }, style]}>
 			<ScrollView style={{ flexGrow: 1, flexShrink: 0 }}>
-				{entries.map(([name, field], index) => (
+				{schema.fieldsArray.map((field, index) => (
 					<FormInput
-						key={name}
-						{...field.inputProps}
-						ref={(element) => {
-							formRefs.current[index] = element;
-						}}
-						type={field.type}
-						value={form[name]}
-						error={errors[name]}
+						key={String(field.name)}
+						type={field.type as never}
+						value={form[field.name]}
+						label={`${field.label}${field.notRequired ? '' : ' *'}`}
+						error={errors[field.name]}
 						hasIcon={hasIcons}
-						button={field.button}
-						blurOnSubmit={index + 1 === entries.length}
-						isLast={index + 1 === entries.length}
-						label={[
-							field.label ?? humanizeToken(name),
-							!field.nullable ? '*' : '',
-						].join(' ')}
+						isLast={index + 1 === schema.fieldsArray.length}
 						disabled={
 							(typeof isBusy === 'function' ? isBusy(form as any) : isBusy) ||
 							isSubmitting ||
-							(field.dependsOn && !form[field.dependsOn]) ||
+							// // (field.dependsOn && !form[field.dependsOn]) ||
 							(typeof status === 'object' && status?.type === 'success')
 						}
-						onSubmitEditing={() => {
-							const nextField = formRefs.current[index + 1];
-							if (!nextField) {
-								handleSubmit();
-								return;
-							}
-							nextField.focus();
-							!nextField.props.editable && Keyboard.dismiss();
-						}}
-						onChange={(value) => {
-							setForm((prev) => ({ ...prev, [name]: value }));
-							onInputChange?.(name, field, value, form as any);
+						onSubmit={handleSubmit}
+						onChange={(value: unknown) => {
+							setForm((prev) => ({ ...prev, [field.name]: value }));
+							onInputChange?.(field, value as never, form);
 						}}
 					/>
 				))}
