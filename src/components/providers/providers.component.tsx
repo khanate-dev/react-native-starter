063df48debd6a054679d8595c1c14516acd9acfd
SuperKXT <superkxt@outlook.com>
2023-06-11 17:12:42 +0500

    Improve `providers.component`

--- src/components/providers/providers.component.tsx
+++ src/components/providers/providers.component.tsx
@@ -1,42 +1,21 @@
 import { PaperProvider } from 'react-native-paper';
 
 import { DarkModeProvider, useDarkMode } from 'contexts/dark-mode';
-import { UserProvider } from 'contexts/user';
+import { AuthProvider } from 'contexts/auth';
 import { AlertProvider } from 'contexts/alert';
 import { LoadingProvider } from 'contexts/loading';
 import { darkTheme, lightTheme } from 'styles/theme';
 
-import type { Dispatch, PropsWithChildren, SetStateAction } from 'react';
-import type { LoggedInUser } from 'schemas/user';
+import type { PropsWithChildren } from 'react';
 
-export type ProvidersProps = PropsWithChildren<{
-	user: null | LoggedInUser;
-	setUser: Dispatch<SetStateAction<null | LoggedInUser>>;
-}>;
-
-const ProvidersSansDarkMode = ({ user, setUser, children }: ProvidersProps) => {
-	const isDarkMode = useDarkMode();
-	return (
-		<PaperProvider theme={isDarkMode ? darkTheme : lightTheme}>
-			<UserProvider
-				user={user}
-				setUser={setUser}
-			>
+export const Providers = ({ children }: PropsWithChildren) => (
+	<DarkModeProvider>
+		<PaperProvider theme={useDarkMode() ? darkTheme : lightTheme}>
+			<AuthProvider>
 				<AlertProvider>
 					<LoadingProvider>{children}</LoadingProvider>
 				</AlertProvider>
-			</UserProvider>
+			</AuthProvider>
 		</PaperProvider>
-	);
-};
-
-export const Providers = ({ children, user, setUser }: ProvidersProps) => (
-	<DarkModeProvider>
-		<ProvidersSansDarkMode
-			user={user}
-			setUser={setUser}
-		>
-			{children}
-		</ProvidersSansDarkMode>
 	</DarkModeProvider>
 );
