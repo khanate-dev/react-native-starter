546b2ddf371bac54cdbff87b21561828c32ffaef
SuperKXT <superkxt@outlook.com>
2023-06-12 17:41:58 +0500

    Remove `CNIC_REGEX` from `constants`

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,4 @@
 export const PHONE_REGEX =
-	/^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/;
+	/^(\+?\d{0,4})?\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{3}\)?)\s?-?\s?(\(?\d{4}\)?)?$/u;
 
-export const JWT_REGEX = /^[0-9a-zA-Z]*\.[0-9a-zA-Z]*\.[0-9a-zA-Z-_]*$/;
-
-export const CNIC_REGEX = /^\d{5}-\d{7}-\d$/;
+export const JWT_REGEX = /^[0-9a-zA-Z]*\.[0-9a-zA-Z]*\.[0-9a-zA-Z-_]*$/u;
