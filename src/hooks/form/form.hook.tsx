86c39fd3e9a7639f771fbbc3c2abad53e6da1288
SuperKXT <superkxt@outlook.com>
2023-06-13 17:50:04 +0500

    Improve `useForm` implementation

--- src/hooks/form/form.hook.tsx
+++ src/hooks/form/form.hook.tsx
@@ -1,16 +1,16 @@
 import { z } from 'zod';
-import { useState } from 'react';
+import { useReducer } from 'react';
 import { Keyboard } from 'react-native';
 
 import { dayjsUtc } from 'helpers/date';
 import { humanizeToken } from 'helpers/string';
 import { shouldAutoFill } from 'src/config';
 import { getCatchMessage } from 'errors/errors';
-import { Alert } from 'components/feedback/alert';
 
 import type { ZodDate, ZodTime } from 'helpers/schema';
 import type { Utils } from 'types/utils';
 import type { ButtonProps } from 'components/controls/button';
+import type { AlertProps } from 'components/feedback/alert';
 
 type fieldMap = {
 	string: z.ZodString;
@@ -67,15 +67,6 @@ const formDefaults: {
 	boolean: false,
 };
 
-type Status<FieldNames extends PropertyKey> =
-	| { type: 'idle' | 'submitting' }
-	| {
-			type: 'error';
-			message: string | null;
-			fieldErrors: Partial<Record<FieldNames, string>>;
-	  }
-	| { type: 'success'; message: string };
-
 type notRequired<T extends fieldZod> = T extends z.ZodNullable<any>
 	? true
 	: T extends z.ZodBoolean
@@ -94,90 +85,147 @@ type Raw<T extends validSchema> = T extends
 	? R
 	: never;
 
+type fieldInput<Zod extends validSchema> = {
+	[k in keyof Raw<Zod>]: {
+		/** the type of the schema field */
+		type: reverseMap<Raw<Zod>[k]>;
+
+		/** will this be the last field in the form? */
+		isLast?: true;
+
+		/** can the field's value be null?  */
+		notRequired?: boolean;
+	} & (notRequired<Raw<Zod>[k]> extends true
+		? { notRequired: true }
+		: { notRequired?: notRequired<Raw<Zod>[k]> });
+};
+
+type State<T extends fieldInput<any>> = {
+	values: { [k in keyof T]: workingTypeMap[T[k]['type']] };
+	status:
+		| { type: 'idle' | 'submitting' }
+		| {
+				type: 'error';
+				message: string | null;
+				fieldErrors: Partial<Record<keyof T, string>>;
+		  }
+		| { type: 'success'; message: string };
+};
+
+type UseFormInput<
+	Zod extends validSchema,
+	FieldInput extends fieldInput<Zod>
+> = {
+	schema: Zod;
+	details: FieldInput;
+	onSubmit: (state: Zod['_output']) => Promise<string | undefined>;
+};
+
 export const useForm = <
 	Zod extends validSchema,
-	FieldInput extends {
-		[k in keyof Raw<Zod>]: {
-			/** the type of the schema field */
-			type: reverseMap<Raw<Zod>[k]>;
-
-			/** can the field's value be null?  */
-			notRequired?: boolean;
-		} & (notRequired<Raw<Zod>[k]> extends true
-			? { notRequired: true }
-			: { notRequired?: notRequired<Raw<Zod>[k]> });
-	}
->(
-	schema: Zod,
-	fieldInput: FieldInput,
-	onSubmit: (state: (typeof schema)['_output']) => Promise<string | undefined>
-) => {
-	const fieldsArray = Object.entries(fieldInput) as [
-		keyof typeof fieldInput,
-		(typeof fieldInput)[keyof typeof fieldInput]
+	Details extends fieldInput<Zod>
+>({
+	schema,
+	details,
+	onSubmit,
+}: UseFormInput<Zod, Details>) => {
+	const fieldsArray = Object.entries(details) as [
+		keyof typeof details,
+		(typeof details)[keyof typeof details]
 	][];
-	const defaultValues = fieldsArray.reduce(
-		(obj, [key, field]) => ({
-			...obj,
-			[key]: shouldAutoFill
-				? formDefaults[field.type]
-				: field.type === 'boolean'
-				? false
-				: ['date', 'time'].includes(field.type)
-				? null
-				: '',
-		}),
-		{}
-	) as { [k in keyof FieldInput]: workingTypeMap[FieldInput[k]['type']] };
 
-	const [state, setState] = useState(defaultValues);
-	const [status, setStatus] = useState<Status<keyof FieldInput>>({
-		type: 'idle',
-	});
+	const [state, dispatch] = useReducer(
+		(
+			old: State<Details>,
+			action:
+				| {
+						type: 'updateState';
+						value: Partial<State<Details>>;
+				  }
+				| { type: 'updateStatus'; value: State<Details>['status'] }
+		): State<Details> => {
+			switch (action.type) {
+				case 'updateState':
+					return {
+						...old,
+						values: { ...old.values, ...action.value },
+					};
+				case 'updateStatus':
+					return { ...old, status: action.value };
+				default:
+					throw new Error('unknown action type');
+			}
+		},
+		{
+			values: fieldsArray.reduce(
+				(obj, [key, field]) => ({
+					...obj,
+					[key]: shouldAutoFill
+						? formDefaults[field.type]
+						: field.type === 'boolean'
+						? false
+						: ['date', 'time'].includes(field.type)
+						? null
+						: '',
+				}),
+				{}
+			) as never,
+			status: { type: 'idle' },
+		}
+	);
 
-	const fields = fieldsArray.reduce(
+	const { values, status } = state;
+
+	const fieldProps = fieldsArray.reduce(
 		(obj, [key, field]) => ({
 			...obj,
 			[key]: {
 				type: field.type,
 				label: humanizeToken(String(key)),
 				notRequired: field.notRequired ?? false,
-				value: state[key],
-				onChange: (value: unknown) => setState({ ...state, [key]: value }),
+				value: values[key],
+				onChange: (value: unknown) =>
+					dispatch({ type: 'updateState', value: { [key]: value } }),
 				error: status.type === 'error' ? status.fieldErrors[key] : undefined,
+				isLast: field.isLast,
+				handleSubmit: field.isLast ? handleSubmit : undefined,
 			},
 		}),
 		{}
 	) as Utils.prettify<{
-		[k in keyof FieldInput]: {
-			type: FieldInput[k]['type'];
-			label: string;
-			notRequired: FieldInput[k]['notRequired'] extends true ? true : false;
-			value: workingTypeMap[FieldInput[k]['type']];
-			onChange: (value: workingTypeMap[FieldInput[k]['type']]) => void;
-			error: string | undefined;
-		};
+		[k in keyof Details]: Utils.prettify<
+			{
+				type: Details[k]['type'];
+				label: string;
+				notRequired: Details[k]['notRequired'] extends true ? true : false;
+				value: workingTypeMap[Details[k]['type']];
+				onChange: (value: workingTypeMap[Details[k]['type']]) => void;
+				error: string | undefined;
+			} & (Details[k]['isLast'] extends true
+				? { isLast: true; onSubmit: () => void }
+				: {})
+		>;
 	}>;
 
-	const statusJsx =
-		(status.type === 'error' || status.type === 'success') && status.message ? (
-			<Alert
-				text={status.message}
-				type={status.type}
-			/>
-		) : null;
+	const statusProps =
+		(status.type === 'error' || status.type === 'success') && status.message
+			? ({ type: status.type, text: status.message } satisfies AlertProps)
+			: null;
 
 	const handleSubmit = async () => {
 		try {
 			Keyboard.dismiss();
-			setStatus({ type: 'submitting' });
+			dispatch({ type: 'updateStatus', value: { type: 'submitting' } });
 
 			const parsed = schema.parse(state);
 			const result = await onSubmit(parsed);
 			const message = result ?? 'Submission Successful!';
 
-			setStatus({ type: 'success', message });
-			setTimeout(() => setStatus({ type: 'idle' }), 2500);
+			dispatch({ type: 'updateStatus', value: { type: 'success', message } });
+			setTimeout(
+				() => dispatch({ type: 'updateStatus', value: { type: 'idle' } }),
+				2500
+			);
 		} catch (error: any) {
 			if (error instanceof z.ZodError) {
 				const fieldErrors = error.issues.reduce(
@@ -191,19 +239,24 @@ export const useForm = <
 					const name = path[0];
 					return (
 						code === 'custom' ||
-						(typeof name === 'string' &&
-							!Object.keys(fieldInput).includes(name))
+						(typeof name === 'string' && !Object.keys(details).includes(name))
 					);
 				});
 				const message = otherIssue
 					? `${otherIssue.path.join('.')}: ${otherIssue.message}`
 					: null;
-				setStatus({ type: 'error', message, fieldErrors });
+				dispatch({
+					type: 'updateStatus',
+					value: { type: 'error', message, fieldErrors },
+				});
 			} else {
-				setStatus({
-					type: 'error',
-					message: getCatchMessage(error),
-					fieldErrors: {},
+				dispatch({
+					type: 'updateStatus',
+					value: {
+						type: 'error',
+						message: getCatchMessage(error),
+						fieldErrors: {},
+					},
 				});
 			}
 		}
@@ -219,10 +272,12 @@ export const useForm = <
 
 	return {
 		state,
-		status,
-		fields,
-		statusJsx,
-		buttonProps,
+		dispatch,
+		props: {
+			field: fieldProps,
+			status: statusProps,
+			button: buttonProps,
+		},
 		handleSubmit,
 	};
 };
