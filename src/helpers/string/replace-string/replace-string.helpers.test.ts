eed323cdbd639322fe7b7e0dc4c86638963dac70
SuperKXT <superkxt@outlook.com>
2023-06-16 17:08:21 +0500

    Update `replace-string` tests

--- src/helpers/string/replace-string/replace-string.helpers.test.ts
+++ src/helpers/string/replace-string/replace-string.helpers.test.ts
@@ -3,11 +3,17 @@ import { z } from 'zod';
 import { replaceString } from './replace-string.helpers';
 
 test('test replaceString', () => {
-	const first = replaceString('example_string_value', '_', ' ');
-	expect(first).toBe('example string value');
-	z.util.assertEqual<typeof first, 'example string value'>(true);
+	const first = {
+		response: replaceString('example_string_value', '_', ' '),
+		expected: 'example string value',
+	} as const;
+	expect(first.response).toBe(first.expected);
+	z.util.assertEqual<typeof first.response, typeof first.expected>(true);
 
-	const second = replaceString('example_string_value', ' ', '-');
-	expect(second).toBe('example_string_value');
-	z.util.assertEqual<typeof second, 'example_string_value'>(true);
+	const second = {
+		response: replaceString('example_string_value', ' ', '-'),
+		expected: 'example_string_value',
+	} as const;
+	expect(second.response).toBe(second.expected);
+	z.util.assertEqual<typeof second.response, typeof second.expected>(true);
 });
