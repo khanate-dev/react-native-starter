b01e1175c3edf486878cbe01cd2fa16d394ccec7
SuperKXT <superkxt@outlook.com>
2023-06-16 18:59:38 +0500

    Update `jest.config.ts`

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,7 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-	testEnvironment: 'jsdom',
 	preset: 'jest-expo',
-	transformIgnorePatterns: [
-		'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
-	],
 	collectCoverage: true,
 	collectCoverageFrom: [
 		'**/*.{js,jsx,ts,tsx}',
