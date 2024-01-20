a625699394bd7a6f3d4a6989b347dc29e730d582
SuperKXT <superkxt@outlook.com>
2024-01-20 21:33:30 +0500

    Update `contexts/auth` to `hooks/user`

---
+++ src/hooks/user.hook.tsx
@@ -0,0 +1,39 @@
+import { useSyncExternalStore } from 'react';
+
+import { Store } from '../helpers/store.helpers.ts';
+import { loggedInUserSchema } from '../schemas/user.schemas.ts';
+
+import type { LoggedInUser } from '../schemas/user.schemas.ts';
+
+export const authStore = new Store({
+	key: 'user',
+	schema: loggedInUserSchema,
+	secureStore: true,
+});
+
+const getUser = () => {
+	return authStore.getSnapShot();
+};
+
+const subscribe = (cb: () => void) => {
+	return authStore.subscribe(cb);
+};
+
+export const logout = async () => {
+	return authStore.remove();
+};
+
+export const login = async (user: LoggedInUser) => {
+	return authStore.set(user);
+};
+
+export const useUserOrNull = () => {
+	const user = useSyncExternalStore(subscribe, getUser);
+	return { user, login, logout, hasInitialized: authStore.hasInitialized };
+};
+
+export const useUser = () => {
+	const user = useSyncExternalStore(subscribe, getUser);
+	if (!user) throw new Error('useUser must be used in an authenticated route');
+	return { user, login, logout, hasInitialized: authStore.hasInitialized };
+};
