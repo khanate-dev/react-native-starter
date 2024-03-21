f98ff692de431f174b932b684303a74146cfd8c4
SuperKXT <superkxt@outlook.com>
2024-03-21 16:24:49 +0500

    Fix lint issues

--- src/content/urdu.content.ts
+++ src/content/urdu.content.ts
@@ -3,41 +3,41 @@
 import type { Content } from '../i18n.ts';
 
 export const urduContent: Content = {
-	title: 'ری ایکٹ نیٹو ابتدائی',
-	headings: {
-		welcome: 'ابتدائی\nری اکٹ نیٹو\nمیں خوش آمدید',
-		login: 'براہ کرم لاگ ان کریں',
-		register: 'خوش آمدید،\nآئیے شروع کرتے ہیں۔',
-	},
 	action: {
+		back: 'واپس',
+		close: 'بند کریں',
+		code: {
+			error: 'دوبارہ بھیجیں',
+			normal: 'کوڈ بھیجیں',
+			success: 'بھیج دیا',
+		},
+		forgotPassword: 'پاسورڈ بھول گئے؟',
 		login: 'داخل ہوں',
 		logout: 'خارج ہوں',
 		register: 'اندراج',
-		close: 'بند کریں',
-		back: 'واپس',
-		forgotPassword: 'پاسورڈ بھول گئے؟',
 		resetPassword: 'پاسورڈ بدلیں',
-		code: {
-			normal: 'کوڈ بھیجیں',
-			error: 'دوبارہ بھیجیں',
-			success: 'بھیج دیا',
-		},
 		verify: {
-			normal: 'تصدیق کریں',
 			error: 'دوبارہ کریں',
+			normal: 'تصدیق کریں',
 			success: 'تصدیق شدہ!',
 		},
 	},
 	error: 'غلطی',
-	oops: 'اوہو',
 	genericError: 'نامعلوم مسئلہ درپیش آیا',
-	resetDescription: 'تبدیلی کے کوڈ کیلئے ای میل درج کریں',
-	resetSent: 'تبدیلی کا کوڈ آپ کو بھیج دیا گیا ہے',
-	pathNotFound: (pathname: string) => `نامعلوم صفحہ: ${pathname}`,
+	headings: {
+		login: 'براہ کرم لاگ ان کریں',
+		register: 'خوش آمدید،\nآئیے شروع کرتے ہیں۔',
+		welcome: 'ابتدائی\nری اکٹ نیٹو\nمیں خوش آمدید',
+	},
+	oops: 'اوہو',
 	pages: {
-		notFound: 'نامعلوم صفحہ',
 		login: 'داخلہ',
+		notFound: 'نامعلوم صفحہ',
 		register: 'اندراج',
 		resetPassword: 'پاسورڈ تبدیلی',
 	},
+	pathNotFound: (pathname: string) => `نامعلوم صفحہ: ${pathname}`,
+	resetDescription: 'تبدیلی کے کوڈ کیلئے ای میل درج کریں',
+	resetSent: 'تبدیلی کا کوڈ آپ کو بھیج دیا گیا ہے',
+	title: 'ری ایکٹ نیٹو ابتدائی',
 };
